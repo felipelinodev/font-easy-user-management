7392e871adb26f0343b3f400562feadaa93a092e
felipelinodev <felipelinodesigndoe@gmail.com>
2025-12-20 19:58:22 -0300

    feat: implement zod for validation

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,42 +1,19 @@
 import { Request, Response } from "express";
 import { userCreate, userDelete } from "../database/UserDao";
-
+import { z } from "zod";
+import { userSchemaZod } from "../schemas/UserSchema";
 
 async function userControllerCreate(req: Request, res: Response){
-    const {name, email, password, photo="minhafoto.com"} = req.body
+    const user = userSchemaZod.safeParse(req.body)
 
-    const plan_type = "Free" 
-
-    const user = {
-        name, 
-        email, 
-        password, 
-        photo, 
-        plan_type
+    if(!user.success){
+        return res.status(400).json({erros: z.flattenError(user.error)})
     }
 
-    if(!name && !email && !password){
-        return res.status(400).json({error: "Nenhum dado preenchido."})
-    }
-
-    if(!name){
-        return res.status(400).json({error: "É necessário adicionar um nome."})
-    }
-
-    if(!email){
-        return res.status(400).json({error: "É necessário adicionar um email."})
-    }
-
-     if(!password){
-       return res.status(400).json({error: "É necessário adicionar uma senha."})
-    }
-
-    await userCreate(user)
+    await userCreate(user.data)
 
     res.status(201).json({message: "usuario criado com sucesso!", user})
 }
-
-
 
 
 async function userControllerDelete(req: Request, res: Response){
---
+++ src/schemas/UserSchema.ts
@@ -0,0 +1,11 @@
+import { z } from "zod";
+
+const userSchemaZod = z.object({
+    name: z.string().min(1, "É necessário adicionar um nome."),
+    email: z.email("Email inválido."),
+    password: z.string().min(6, "A senha deve ter no mínimo 6 caracteres."),
+    photo: z.url().optional().default("minhafoto.com"),
+    plan_type: z.string().optional().default("FREE")
+})
+
+export { userSchemaZod }
