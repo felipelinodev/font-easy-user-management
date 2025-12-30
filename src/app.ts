3e14483ef741babad1e072454118a611b922e407
felipelinodev <felipelinodesigndoe@gmail.com>
2025-12-29 21:43:39 -0300

    fix: try resolve cors in cookes write

--- src/app.ts
+++ src/app.ts
@@ -7,15 +7,17 @@ const app = express();
 
 app.set('trust proxy', 1);
 
+app.use(cors({
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true,
+  optionsSuccessStatus: 200
+}))
+
 
 app.use(express.json());
 app.use(cookieParser())
 
 
-app.use(cors({
-  origin: process.env.FRONTEND_URL,
-  credentials: true,
-}))
 
 app.use(router);
 
