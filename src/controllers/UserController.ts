58d4e6ca1498bb1df43af50a85d58ad502d806bf
felipelinodev <felipelinodesigndoe@gmail.com>
2025-12-29 23:44:11 -0300

    fix: last try

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -51,8 +51,8 @@ async function userGoogleControllerLogin(req: Request, res: Response) {
 
     res.cookie('font-easy-auth', tokenAuth, {
         httpOnly: true,
-        secure: false, // ⚠️ EM PRODUÇÃO: secure: true (HTTPS obrigatório)
-        sameSite: 'lax', // ⚠️ EM PRODUÇÃO: 'none' (front e back em domínios diferentes)
+        secure: true, // ⚠️ EM PRODUÇÃO: secure: true (HTTPS obrigatório)
+        sameSite: 'none', // ⚠️ EM PRODUÇÃO: 'none' (front e back em domínios diferentes)
         maxAge: 7 * 24 * 60 * 60 * 1000,
         path: '/',
     })
@@ -77,13 +77,15 @@ async function userControllerLogin(req: Request, res: Response){
     const tokenAuth = generateToken(loginData.id)
 
    
+   
     res.cookie('font-easy-auth', tokenAuth, {
         httpOnly: true,
-        secure: false, // ⚠️ DEV (localhost). EM PRODUÇÃO: secure: true (HTTPS obrigatório)
-        sameSite: 'lax', // ⚠️ DEV. EM PRODUÇÃO: 'none' (front e back em domínios diferentes)
+        secure: true, // ⚠️ EM PRODUÇÃO: secure: true (HTTPS obrigatório)
+        sameSite: 'none', // ⚠️ EM PRODUÇÃO: 'none' (front e back em domínios diferentes)
         maxAge: 7 * 24 * 60 * 60 * 1000,
         path: '/',
-        })
+    })
+
 
     res.status(200).json({message: "Login realizado com sucesso."})
 }
